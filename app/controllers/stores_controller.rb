e77add0e1fefbe4f73741d126b775da473924a99
Aerin Jang <205189969+Xerin-J@users.noreply.github.com>
2025-08-12 22:06:37 +0900

    Controller, routes and view done for store

---
+++ app/controllers/stores_controller.rb
@@ -0,0 +1,29 @@
+class StoresController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item, only: [:show, :purchase]
+
+  def list_items
+    @items = Item.all
+  end
+
+  def show
+    @item
+  end
+
+  def purchase
+    if current_user.coins >= @item.price
+      current_user.coins -= @item.price
+      current_user.save
+      flash[:notice] = "You've purchased #{@item.name}!"
+    else
+      flash[:alert] = "You don't have enough coins to buy #{@item.name}."
+    end
+    redirect_to list_items_store_path
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,17 @@ Rails.application.routes.draw do
 
   resources :characters, only: [:index, :new, :create, :update, :destroy]
 
+  resource :store, path: "thewanderingsatchel", only: [:show] do
+    collection do
+      get :list_items
+    end
+    member do
+      post :purchase_items
+    end
+  end
+
+  resources :items, only: [:show]
+
 
   root to: "pages#home"
   get "up" => "rails/health#show", as: :rails_health_check
