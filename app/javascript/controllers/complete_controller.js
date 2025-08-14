81d51e11803f6e69d07c9e005b9ec0e5b562c7c8
Antonio Vinciguerra <anto.vinciguerra@hotmail.com>
2025-08-14 16:19:48 +0900

    fixed the patch request for completed quests

--- app/javascript/controllers/complete_controller.js
+++ app/javascript/controllers/complete_controller.js
@@ -4,23 +4,6 @@ export default class extends Controller {
   static targets = ["button"]
 
   markCompleted(event) {
-    event.preventDefault();
-
-    const url = this.buttonTarget.dataset.url;
-
-    fetch(url, {
-      method: "PATCH",
-      headers: {
-        "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]').content,
-        "Accept": "application/json",
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ quest: { completed: true } })
-    }).then(response => {
-      if (response.ok) {
-        this.buttonTarget.textContent = "Completed";
-        this.buttonTarget.disabled = true;
-      }
-    });
+    this.buttonTarget.textContent = "Completed";
   }
 }
