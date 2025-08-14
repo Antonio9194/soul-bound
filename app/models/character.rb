7c0d33a7ed40531decf449398c0296fcff0474cc
Antonio Vinciguerra <anto.vinciguerra@hotmail.com>
2025-08-14 15:30:34 +0900

    minor change

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,6 +1,7 @@
 class Character < ApplicationRecord
   belongs_to :user
   has_many :inventory_items
+  has_many :quests
 
   validates :name, presence: true, length: { minimum:1, maximum:100 }
   validates :class_name, :gender, :apperance, presence: true
