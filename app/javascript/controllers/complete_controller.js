00065937c6e49f2d7e513e9dca15c7d5f3c7c1dc
Antonio Vinciguerra <anto.vinciguerra@hotmail.com>
2025-08-14 17:36:25 +0900

    added logic for a rewards popup

--- app/javascript/controllers/complete_controller.js
+++ app/javascript/controllers/complete_controller.js
@@ -1,9 +1,25 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["button"]
+  static targets = ["button", "popup"]
 
   markCompleted(event) {
+
     this.buttonTarget.textContent = "Completed";
+
+    if (this.hasPopupTarget) {
+      this.display();
+    }
+  }
+  
+  display() {
+    this.popupTarget.style.display = "block";
+    setTimeout(() => {
+      this.close();
+    }, 9000); 
+  }
+
+  close() {
+    this.popupTarget.style.display = "none";
   }
 }
