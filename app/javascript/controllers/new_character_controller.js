7d8003d4be6c719e976403564125e496de4cb57e
Antonio Vinciguerra <anto.vinciguerra@hotmail.com>
2025-08-18 12:12:38 +0900

    done

--- app/javascript/controllers/new_character_controller.js
+++ app/javascript/controllers/new_character_controller.js
@@ -7,14 +7,17 @@ export default class extends Controller {
     const gender = this.genderTargets.find(r => r.checked)?.value
     const appearance = this.appearanceTargets.find(r => r.checked)?.value
     const klass = this.klassTargets.find(r => r.checked)?.value
-    if (!gender || !appearance || !klass) return
 
-    // Build the image paths
+    if (!gender || !appearance || !klass) {
+      this.displayTarget.innerHTML = ""
+      return
+    }
+
     const appearancePath = `/assets/ph${gender}${appearance}.png`
     const classPath = `/assets/${klass}.png`
 
     this.displayTarget.innerHTML = `
-      <img src="${appearancePath}" alt="Character" style="position: absolute; width: 160px; height: auto;" />
+      <img src="${appearancePath}" alt="Character Appearance" style="position: absolute; width: 160px; height: auto;" />
       <img src="${classPath}" alt="${klass}" style="position: absolute; width: 160px; height: auto;" />
     `
   }
