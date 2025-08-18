f7d28d073f3752e4f65730cc5bd087022e7f8cf4
Antonio Vinciguerra <anto.vinciguerra@hotmail.com>
2025-08-18 12:05:52 +0900

    changed stimulus contr

--- app/javascript/controllers/new_character_controller.js
+++ app/javascript/controllers/new_character_controller.js
@@ -7,12 +7,13 @@ export default class extends Controller {
     const gender = this.genderTargets.find(r => r.checked)?.value
     const appearance = this.appearanceTargets.find(r => r.checked)?.value
     const klass = this.klassTargets.find(r => r.checked)?.value
-
     if (!gender || !appearance || !klass) return
 
-    const fileName = `${klass}.png`
-    const imgPath = `/assets/${fileName}`
+    const basePath = this.displayTarget.dataset.baseImagePath
 
-    this.displayTarget.innerHTML = `<img src="${imgPath}" alt="${klass}" style="width: 100%; height: auto;" />`
+    this.displayTarget.innerHTML = `
+      <img src="${basePath}ph${gender}${appearance}.png" alt="Character" style="position: absolute; width: 160px; height: auto;" />
+      <img src="${basePath}${klass}.png" alt="${klass}" style="position: absolute; width: 160px; height: auto;" />
+    `
   }
 }
