fe2003fb1a7e3bffb3b0cfd4f8974b76d6e3d65e
Antonio Vinciguerra <anto.vinciguerra@hotmail.com>
2025-08-18 11:23:37 +0900

    fix typo

--- app/javascript/controllers/new_character_controller.js
+++ app/javascript/controllers/new_character_controller.js
@@ -1,20 +1,16 @@
-import { Controller } from "@hotwired/stimulus"
+static targets = ["apperance", "gender", "klass", "display"]
 
-// Connects to data-controller="new-character"
-export default class extends Controller {
-  static targets = ["appearance", "gender", "klass", "display"]
-
-  render(event) {
+render(event) {
   const klass = this.klassTargets.find(radio => radio.checked)?.value
   const gender = this.genderTargets.find(radio => radio.checked)?.value
-  const appearance = this.appearanceTargets.find(radio => radio.checked)?.value
+  const apperance = this.apperanceTargets.find(radio => radio.checked)?.value
 
   const basePath = this.displayTarget.dataset.baseImagePath
 
-  let genderAppearance = ""
-  if (gender && appearance) {
-    genderAppearance = `
-      <img style="position: absolute" src="${basePath}ph${gender}${appearance}.png" alt="">
+  let genderApperance = ""
+  if (gender && apperance) {
+    genderApperance = `
+      <img style="position: absolute" src="${basePath}ph${gender}${apperance}.png" alt="">
     `
   }
 
@@ -25,6 +21,5 @@ export default class extends Controller {
     `
   }
 
-  this.displayTarget.innerHTML = `${genderAppearance}${klassTarget}`
-}
+  this.displayTarget.innerHTML = `${genderApperance}${klassTarget}`
 }
