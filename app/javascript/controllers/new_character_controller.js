cc621c6b08b7d2297cdff8146909e56f25646ed9
Antonio Vinciguerra <anto.vinciguerra@hotmail.com>
2025-08-18 11:28:25 +0900

    typo

--- app/javascript/controllers/new_character_controller.js
+++ app/javascript/controllers/new_character_controller.js
@@ -1,25 +1,27 @@
-static targets = ["apperance", "gender", "klass", "display"]
+export default class NewCharacterController {
+  static targets = ["apperance", "gender", "klass", "display"]
 
-render(event) {
-  const klass = this.klassTargets.find(radio => radio.checked)?.value
-  const gender = this.genderTargets.find(radio => radio.checked)?.value
-  const apperance = this.apperanceTargets.find(radio => radio.checked)?.value
+  render() {
+    const klass = this.klassTargets.find(radio => radio.checked)?.value
+    const gender = this.genderTargets.find(radio => radio.checked)?.value
+    const apperance = this.apperanceTargets.find(radio => radio.checked)?.value
 
-  const basePath = this.displayTarget.dataset.baseImagePath
+    const basePath = this.displayTarget.dataset.baseImagePath
 
-  let genderApperance = ""
-  if (gender && apperance) {
-    genderApperance = `
-      <img style="position: absolute" src="${basePath}ph${gender}${apperance}.png" alt="">
-    `
+    let genderApperance = ""
+    if (gender && apperance) {
+      genderApperance = `
+        <img style="position: absolute" src="${basePath}ph${gender}${apperance}.png" alt="">
+      `
+    }
+
+    let klassTarget = ""
+    if (klass) {
+      klassTarget = `
+        <img style="position: absolute" src="${basePath}${klass}.png" alt="">
+      `
+    }
+
+    this.displayTarget.innerHTML = `${genderApperance}${klassTarget}`
   }
-
-  let klassTarget = ""
-  if (klass) {
-    klassTarget = `
-      <img style="position: absolute" src="${basePath}${klass}.png" alt="">
-    `
-  }
-
-  this.displayTarget.innerHTML = `${genderApperance}${klassTarget}`
 }
