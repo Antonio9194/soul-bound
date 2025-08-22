a16feae1af113fb540c4805a03e57a96ed168811
Antonio Vinciguerra <anto.vinciguerra@hotmail.com>
2025-08-22 14:11:35 +0900

    working purchase method and inventory items display

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -12,14 +12,21 @@ class StoresController < ApplicationController
   end
 
   def purchase
-    if current_user.character.coin >= @item.price
-      current_user.character.coin -= @item.price
-      current_user.character.save
-      flash[:notice] = "You've purchased #{@item.name}!"
+    if current_user.character.inventory_items.exists?(item_id: @item.id)
+      flash[:alert] = "You already own this item!"
     else
-      flash[:alert] = "You don't have enough coins to buy #{@item.name}."
+      if current_user.character.coin >= @item.price
+        current_user.character.coin -= @item.price
+        current_user.character.save
+        # Add purchased item to inventory
+        current_user.character.inventory_items.create!(item: @item, equipped: false)
+
+        flash[:notice] = "You've purchased #{@item.name}!"
+      else
+        flash[:alert] = "You don't have enough coins to buy #{@item.name}."
+      end
     end
-    redirect_to list_items_store_path
+    redirect_to list_items_stores_path
   end
 
   private
