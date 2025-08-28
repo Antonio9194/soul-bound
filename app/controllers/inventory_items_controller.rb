5791ce47eba3a482808527a9b5b913d6af15bff3
Antonio Vinciguerra <anto.vinciguerra@hotmail.com>
2025-08-28 12:25:41 +0900

    minor fix

--- app/controllers/inventory_items_controller.rb
+++ app/controllers/inventory_items_controller.rb
@@ -37,7 +37,8 @@ def equip
     equipped_item = current_user.character.inventory_items
                         .includes(:item)
                         .where(equipped: true)
-                        .find { |inv| slot_mapping[slot].any? { |name| inv.item.img.include?(name) } }
+                        .find { |inv| slot_mapping[slot].any?
+                        { |name| inv.item.img.include?(name) } }
 
     equipped_item&.update(equipped: false)
   end
