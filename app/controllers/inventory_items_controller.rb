957f0d1310c2eb7189a6d18620380516bf2dcddb
Antonio Vinciguerra <anto.vinciguerra@hotmail.com>
2025-08-28 12:08:53 +0900

    updated equip method with a swap function

--- app/controllers/inventory_items_controller.rb
+++ app/controllers/inventory_items_controller.rb
@@ -22,11 +22,30 @@ class InventoryItemsController < ApplicationController
   def show
   end
 
-  def equip
-    @inventory_item = InventoryItem.find(params[:id])
-    @inventory_item.update(equipped: true)
-    redirect_to character_path, notice: "#{@inventory_item.item.name} equipped!"
+def equip
+  slot_mapping = {
+    "head" => ["head"],
+    "top" => ["top"],
+    "bottom" => ["bottom"],
+    "weapon" => ["sword", "staff", "book"],
+    "accessory" => ["ring", "amulet", "potion"]
+  }
+
+  slot = slot_mapping.find { |_slot, names| names.any? { |name| @inventory_item.item.img.include?(name) } }&.first
+
+  if slot
+    # unequip currently equipped item in the same slot
+    equipped_item = current_user.character.inventory_items
+                        .includes(:item)
+                        .where(equipped: true)
+                        .find { |inv| slot_mapping[slot].any? { |name| inv.item.img.include?(name) } }
+
+    equipped_item&.update(equipped: false)
   end
+
+  @inventory_item.update(equipped: true)
+  redirect_to character_path, notice: "#{@inventory_item.item.name} equipped#{slot ? " in #{slot}" : ""}!"
+end
 
   def unequip
     @inventory_item = InventoryItem.find(params[:id])
