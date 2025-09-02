9aa9d07535bcbdcd8d5c6fdb20e504889e20acaa
Antonio Vinciguerra <anto.vinciguerra@hotmail.com>
2025-09-02 22:14:10 +0900

    done

--- app/javascript/controllers/accept_controller.js
+++ app/javascript/controllers/accept_controller.js
@@ -5,20 +5,24 @@ export default class extends Controller {
   static targets = ["button", "popup"]
 
   markAccepted(event) { 
+    
+    
 
     const btn = this.buttonTarget
     btn.innerText = "Accepted"
     btn.disabled = true
     btn.classList.remove("text-active")
     btn.classList.add("text-completed")
-     // Submit the form to Rails
+     
     const audio = document.getElementById("complete-audio")
-
+    console.log(this.popupTarget);
     // Show Bootstrap modal
     if (this.hasPopupTarget) {
       const modal = new bootstrap.Modal(this.popupTarget)
       modal.show()
       audio.play()
+    } else {
+      "noPopupTarget"
     }
   }
 }
